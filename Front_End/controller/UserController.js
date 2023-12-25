62d318a751d3184232236997691abcb123f698e3
Hirusha2000 <2000hirushaudara@gmail.com>
2023-12-25 19:38:32 +0530

    new updates

--- Front_End/controller/UserController.js
+++ Front_End/controller/UserController.js
@@ -243,7 +243,7 @@ const regExEmailCusAddress = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const regExNIC = /^([0-9]{12}|[0-9V]{10})$/;
 const regExDrivingNIC = /^[A-Z0-9-]+$/;
 const regExUserName = /^[A-z0-9/ ]{4,30}$/;
-const regExPassword = /^([A-Z a-z]{5,15}[0-9]{1,10})$/;
+const regExPassword = /^[A-z0-9/ ]{4,30}$/;
 
 let customerValidations = [];
 customerValidations.push({
